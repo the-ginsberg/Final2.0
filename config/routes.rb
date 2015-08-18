5b3f2f228b5d1cb5d4348a8f42a57a03e2364f77
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-18 18:11:50 -0400

    Newsfeed controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'newsfeed/index'
+  get 'newsfeed/show'
+  get 'newsfeed/new'
+  get 'newsfeed/edit'
+  get 'newsfeed/create'
+  get 'newsfeed/update'
+  get 'newsfeed/destroy'
+
   get 'dashboard/index'
   get 'dashboard/show'
   get 'dashboard/new'
   post 'dashboards' => 'dashboard#create'
-  post 'dashboards/create'
   get 'dashboard/edit'
   resources :dashboard do
     resources :newsfeed
