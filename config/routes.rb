7bfb1aa6422c9eeaf27469dd957e467ef7a04f7a
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-18 16:55:39 -0400

    Dashboard Controller Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
+  get 'dashboard/show'
+
+  get 'dashboard/new'
+
+  get 'dashboard/edit'
+
   devise_for :users
   root 'user#home'
 end
