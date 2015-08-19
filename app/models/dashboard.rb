f27e310a62cf1e0a15d74359f63d46dcc7f9cb86
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-19 12:44:04 -0400

    fixed delete method

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,6 +1,6 @@
 class Dashboard < ActiveRecord::Base
   has_many :users
-  has_many :newsfeeds
+  has_many :newsfeeds, :dependent => :delete_all
   validates :user_id, presence: true
 
   def set_user(user)
